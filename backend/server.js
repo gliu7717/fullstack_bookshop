9f5e50f2a89410bdf6e2cddbcc921ddb0dfc642f
rayliu7717 <rayliu2004@gmail.com>
2024-05-07 15:39:28 -0400

    add bookroutes and get books from database

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js';
-import books from './data/books.js';
+import bookRoutes from './routes/bookRoutes.js';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
 
-app.get('/api/books', (req, res) => {
-  res.json(books);
-});
 
-app.get('/api/books/:id', (req, res) => {
-  const book = books.find((p) => p._id === req.params.id);
-  res.json(book);
-});
+app.use('/api/books', bookRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
