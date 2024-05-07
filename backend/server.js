0943c45a8cf53f721624c5795e7bf53efca3be5a
rayliu7717 <rayliu2004@gmail.com>
2024-05-07 09:23:32 -0400

    add mongoose model data

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  );
