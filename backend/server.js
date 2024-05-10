9d89057dd217da080e28e240d84a3bdeebd47cca
rayliu7717 <rayliu2004@gmail.com>
2024-05-09 21:28:21 -0400

    User Routes and Controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js';
 import bookRoutes from './routes/bookRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ const app = express();
 
 
 app.use('/api/books', bookRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
