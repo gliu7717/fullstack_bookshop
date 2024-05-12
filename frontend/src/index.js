3ed826cb5961d1fa5c0046594e62312f6508d28a
rayliu7717 <rayliu2004@gmail.com>
2024-05-12 16:07:16 -0400

    add shippingScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import BookScreen from './screens/BookScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 import store from './store';
 import { Provider } from 'react-redux';
 
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      <Route path='/shipping' element={<ShippingScreen />} />
     </Route>
   )
 );
