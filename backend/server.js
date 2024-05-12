1f3c9ea3700ddb73939272ab97c622d58f724449
rayliu7717 <rayliu2004@gmail.com>
2024-05-12 17:50:03 -0400

    add orders in backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config()
 import connectDB from './config/db.js';
 import bookRoutes from './routes/bookRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
@@ -18,6 +19,7 @@ app.use(cookieParser());
 
 app.use('/api/books', bookRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
